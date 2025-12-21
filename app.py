b6c8a291537839ed0e67be8f97f2aa006803249c
wataila <wataila05@gmail.com>
2025-12-21 16:11:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ if section =='About Me':
 
     st.markdown("""
     ### About Me
-    hhhhhhh
+    I am a third year aerospace engineering student at Illinois Institute of Technology pursing both a bachelors and masters degree both of which I will be graduating with in the spring of 2027
 
     ### Links
     - [LinkedIn](https://www.linkedin.com/in/william-taila-594545305/)
