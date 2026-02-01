cd1faac8617e4ec870e98bc9aa444ddfb663e20a
wataila <wataila05@gmail.com>
2026-01-31 19:21:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ elif section == 'Solidworks/CAD Designs':
         """)
 
     with col2:
-        st.image("assets/Screenshot 2026-01-31 190504.png", use_container_width=True)
+        st.image("assets/Screenshot 2026-01-31 190504.png", width=250)
 
 
 elif section == 'AIAA Spacecraft Design Competition':
