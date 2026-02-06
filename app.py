8c64cf499bf0c1d5bed80f3e40289a48075e2d0f
wataila <wataila05@gmail.com>
2026-02-05 18:50:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ elif section == 'Solidworks/CAD Designs':
         This lattice pencil holder was modeled in SolidWorks by following and adapting an online tutorial to practice advanced CAD techniques. The design was created to better understand helical patterning, surface features, and parametric control in complex geometry. Modeling this part helped reinforce SolidWorks workflow and feature management through hands-on application.
         """)
     with col2:
-        st.image("Screenshot 2026-02-05 184013.png")
+        st.image("assets/Screenshot 2026-02-05 184013.png")
 
 
 elif section == 'AIAA Spacecraft Design Competition':
